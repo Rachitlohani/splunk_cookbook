b5573cc8173cdfa9e94249d980de2e938e0890c1
eric.miller <eric.miller3@bestbuy.com>
2015-12-04 14:14:54 -0600

    Updated serverspec for server/forwarder suites

--- test/integration/forwarder/serverspec/splunk_forwarder_spec.rb
+++ test/integration/forwarder/serverspec/splunk_forwarder_spec.rb
@@ -3,15 +3,50 @@ require 'serverspec'
 set :backend, :exec
 
 describe 'Splunk Forwarder' do
+  describe user('splunk') do
+    it { should exist }
+    it { should belong_to_group 'splunk' }
+    it { should have_uid 396 }
+    it { should have_home_directory '/opt/splunkforwarder' }
+    it { should have_login_shell '/bin/bash' }
+  end
+
+  describe group('splunk') do
+    it { should exist }
+    it { should have_gid 396 }
+  end
+
   describe service('splunk') do
+    it { should be_enabled }
     it { should be_running }
   end
 
-  describe command('/opt/splunkforwarder/bin/splunk version') do
-    its(:stdout) { should match "Splunk Universal Forwarder 6.3.1 (build f3e41e4b37b2)\n" }
+  describe process('splunkd') do
+    its(:user) { should match 'root' }
   end
 
-  ['outputs', 'limits'].each do |conf_file|
+  describe file('/opt/splunkforwarder/etc/splunk-launch.conf') do
+    it { should exist }
+    it { should be_file }
+    its(:content) { should match 'SPLUNK_HOME=/opt/splunkforwarder' }
+    its(:content) { should match 'SPLUNK_OS_USER=root' }
+  end
+
+  describe file('/opt/splunkforwarder/etc/.setup_admin_pwd') do
+    it { should exist }
+  end
+
+  describe file('/opt/splunk_setup_passwd') do
+    it { should_not exist }
+  end
+
+  describe file('/opt/splunkforwarder/etc/splunk.version') do
+    it { should exist }
+    its(:content) { should match 'VERSION=6.3.1' }
+    its(:content) { should match 'BUILD=f3e41e4b37b2' }
+  end
+
+  %w(outputs limits).each do |conf_file|
     describe file("/opt/splunkforwarder/etc/system/local/#{conf_file}.conf") do
       it { should exist }
     end
--- test/integration/server/serverspec/splunk_server_spec.rb
+++ test/integration/server/serverspec/splunk_server_spec.rb
@@ -2,7 +2,30 @@ require 'serverspec'
 
 set :backend, :exec
 
+
 describe 'Splunk Server' do
+  describe user('splunk') do
+    it { should exist }
+    it { should belong_to_group 'splunk' }
+    it { should have_uid 396 }
+    it { should have_home_directory '/opt/splunk' }
+    it { should have_login_shell '/bin/bash' }
+  end
+
+  describe group('splunk') do
+    it { should exist }
+    it { should have_gid 396 }
+  end
+
+  describe service('splunk') do
+    it { should be_enabled }
+    it { should be_running }
+  end
+
+  describe process('splunkd') do
+    its(:user) { should match 'root' }
+  end
+
   describe port(80) do
     it { should be_listening }
   end
@@ -11,31 +34,40 @@ describe 'Splunk Server' do
     it { should be_listening }
   end
 
-  describe service('splunk') do
-    it { should be_running }
+  describe file('/opt/splunk/etc/splunk-launch.conf') do
+    it { should exist }
+    it { should be_file }
+    its(:content) { should match 'SPLUNK_HOME=/opt/splunk' }
+    its(:content) { should match 'SPLUNK_OS_USER=root' }
   end
 
-  describe file('/volr/splunk') do
-    it { should be_directory }
+  describe file('/opt/splunk/etc/.setup_admin_pwd') do
+    it { should exist }
   end
 
-  describe command('/opt/splunk/bin/splunk version') do
-    its(:stdout) { should match "Splunk 6.3.1 (build f3e41e4b37b2)\n" }
+  describe file('/opt/splunk_setup_passwd') do
+    it { should_not exist }
   end
 
-  ['apache_http', 'useragents'].each do |dashboard|
+  describe file('/opt/splunk/etc/splunk.version') do
+    it { should exist }
+    its(:content) { should match 'VERSION=6.3.1' }
+    its(:content) { should match 'BUILD=f3e41e4b37b2' }
+  end
+
+  %w(apache_http useragents).each do |dashboard|
     describe file("/opt/splunk/etc/users/admin/search/local/data/ui/views/#{dashboard}.xml") do
       it { should exist }
     end
   end
 
-  ['web', 'transforms', 'limits', 'indexes'].each do |conf_file|
+  %w(web transforms limits indexes).each do |conf_file|
     describe file("/opt/splunk/etc/system/local/#{conf_file}.conf") do
       it { should exist }
     end
   end
 
-  ['inputs', 'props'].each do |conf_file|
+  %w(inputs props).each do |conf_file|
     describe file("/opt/splunk/etc/system/local/#{conf_file}.conf") do
       it { should exist }
     end
